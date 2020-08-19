e4cbd292ed3c7000c0a0bb842776e174755c5464
Atsamaz Gatsoev <dmr0@protonmail.com>
2020-08-19 16:42:37 +0300

    add function description

--- index.js
+++ index.js
@@ -6,15 +6,22 @@
 const serve = require('koa-static')
 const path = require('path')
 
-module.exports = (hostnames, app, static_dir) => {
+/* Arguments
+ * list of hostnames (you need to create folders in static directory with the same names) - required
+ * Koa app object - required
+ * path to your static directory - required
+ * path to default folder (in case if none of hostnames matched this one will be used) - not required
+*/
+
+module.exports = (hostnames, app, static_dir, default_hostname = '') => {
     return async (ctx, next) => {
         try {
             // If there is a koa-static middleware get its index
             const serve_index = app.middleware.findIndex(func => func.name === 'serve')
-            // Skip if no hostname matches
-            if (!hostnames.includes(ctx.hostname)) return await next()
+            // Use default hostname if no matches
+            const hostname = hostnames.includes(ctx.hostname) ? ctx.hostname : default_hostname
             // Compose path to static folder using hostname (must be the same as hostname)
-            const static_middleware = serve(path.join(__dirname, static_dir, ctx.hostname))
+            const static_middleware = serve(path.join(__dirname, static_dir, hostname))
 
             if (serve_index > -1) {
                 // Replace existing koa-static with the new one
